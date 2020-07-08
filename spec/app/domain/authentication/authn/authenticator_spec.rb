1d0eee3598d98f2e5a1b4ecc91f58c5f807b8a13
Eran Hadar <eran.hadar@cyberark.com>
2020-07-08 18:48:57 +0300

    Code review comments fixed

--- spec/app/domain/authentication/authn/authenticator_spec.rb
+++ spec/app/domain/authentication/authn/authenticator_spec.rb
@@ -3,86 +3,56 @@
 require 'spec_helper'
 
 RSpec.describe 'Authentication::Authn::Authenticator' do
-  let(:input) do
-    ::Authentication::AuthenticatorInput.new(
-      authenticator_name: 'authn',
-      service_id:         'service',
-      account:            'account',
-      username:           'username',
-      credentials:        'creds',
-      client_ip:          '127.0.0.1',
-      request:            nil
-    )
-  end
+  include_context "authn setup"
 
-  let(:mocked_role) { double("Role") }
-  def mocked_role
-    double('Role').tap do |role|
-      allow(role).to receive(:roleid_from_username)
-                       .and_return("account:user:username")
-    end
-   end
-
-  let(:mocked_credentials) { double("Credentials") }
-  def mocked_credentials
-    double('Credentials').tap do |creds|
-      allow(creds).to receive(:[]).and_return(creds)
-      allow(creds).to receive(:valid_api_key?).and_return(true)
-    end
-  end
-
-  let(:mocked_role_not_found_credentials) { double("Credentials") }
-  def mocked_role_not_found_credentials
-    double('Credentials').tap do |creds|
-      allow(creds).to receive(:[]).and_return(nil)
-    end
-  end
-
-  let(:mocked_invalid_api_key_credentials) { double("Credentials") }
-  def mocked_invalid_api_key_credentials
-    double('Credentials').tap do |creds|
-      allow(creds).to receive(:[]).and_return(creds)
-      allow(creds).to receive(:valid_api_key?).and_return(false)
-    end
-  end
-
-  context "An authn/authenticator" do
+  context "An Authn authenticator" do
     context "that receives an authentication request" do
-      context "with a valid credentials" do
+      context "with valid credentials" do
         subject do
            ::Authentication::Authn::Authenticator.new(
                 role_cls:  mocked_role,
-                credentials_cls: mocked_credentials,).call(authenticator_input: input)
+                credentials_cls: mocked_valid_api_key_credentials
+           ).call(
+             authenticator_input: input
+           )
         end
 
         it "does not raise an error" do
           expect {subject}.to_not raise_error
         end
 
-        it "should return true" do
+        it "should returns true" do
           expect(subject).to eq(true)
         end
       end
-      context "with role not found" do
+
+      context "with non-existing role" do
         subject do
           ::Authentication::Authn::Authenticator.new(
               role_cls:  mocked_role,
-              credentials_cls: mocked_role_not_found_credentials,).call(authenticator_input: input)
+              credentials_cls: mocked_role_not_found_credentials
+          ).call(
+            authenticator_input: input
+          )
         end
 
         it "does not raise an error" do
           expect {subject}.to_not raise_error
         end
 
-        it "should return false" do
+        it "should returns false" do
           expect(subject).to eq(false)
         end
       end
+
       context "with an invalid api key" do
         subject do
           ::Authentication::Authn::Authenticator.new(
             role_cls:  mocked_role,
-            credentials_cls: mocked_invalid_api_key_credentials,).call(authenticator_input: input)
+            credentials_cls: mocked_invalid_api_key_credentials
+          ).call(
+            authenticator_input: input
+          )
         end
 
         it "does not raise an error" do
--- spec/app/domain/authentication/authn/login_spec.rb
+++ spec/app/domain/authentication/authn/login_spec.rb
@@ -1,53 +1,20 @@
-
 # frozen_string_literal: true
 
 require 'spec_helper'
+
 RSpec.describe 'Authentication::Authn::Login' do
-  let(:input) do
-    ::Authentication::AuthenticatorInput.new(
-      authenticator_name: 'authn',
-      service_id:         'service',
-      account:            'account',
-      username:           'username',
-      credentials:        'creds',
-      client_ip:          '127.0.0.1',
-      request:            nil
-    )
-  end
+  include_context "authn setup"
 
-  let(:mocked_role) { double("Role") }
-  def mocked_role
-    double('Role').tap do |role|
-      allow(role).to receive(:roleid_from_username)
-                       .and_return("account:user:username")
-    end
-  end
-
-  let(:mocked_credentials) { double("Credentials") }
-  def mocked_credentials
-    double('Credentials').tap do |creds|
-      allow(creds).to receive(:[]).and_return(creds)
-      allow(creds).to receive(:authenticate).and_return(true)
-      allow(creds).to receive(:api_key).and_return('valid_api_key')
-    end
-  end
-
-  let(:mocked_role_not_found_credentials) { double("Credentials") }
-  def mocked_role_not_found_credentials
-    double('Credentials').tap do |creds|
-      allow(creds).to receive(:[]).and_return(nil)
-    end
-  end
-
-  context "An authn/login" do
-    context "that receives a login input" do
+  context "An An Authn authenticator" do
+    context "that receives login request" do
       context "with a valid credentials" do
         subject do
           ::Authentication::Authn::Login.new(
               role_cls:  mocked_role,
               credentials_cls: mocked_credentials
+          ).call(
+            authenticator_input: input
           )
-          .call(authenticator_input: input)
         end
 
         it "does not raise an error" do
